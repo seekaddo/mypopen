f3b7f1dff96b330f552ae8dd5bd81a9dc5be8c23
seekaddo <dennis_addo@aol.com>
2017-04-17 04:05:32 +0200

    update

--- mypopen.c
+++ mypopen.c
@@ -6,16 +6,10 @@
  * \param type
  * */
 
-#if 0
+#if 1
 #define tpsr(a,b) (*type == 'r' ? (b) : (a))
 #define tpsw(a,b) (*type == 'w' ? (b) : (a))
-
-#else
-
-#define tpsrw(a,b) (*type == 'r' ? (b) : (a))
-
 #endif
-
 /** Robert use this to help you in your implementation
  * */
 static FILE *fp_stream = NULL;
@@ -25,7 +19,9 @@ static const char * const shell_path = "/bin/sh";
 
 FILE *mypopen(const char *command, const char *type) {
 
-    int pipe_list[2];
+    int fileDis[2];
+    //int read_fd = 0;
+    //int write_fd = 0;
     int parent_fd, child_fd;
 
 
@@ -34,19 +30,24 @@ FILE *mypopen(const char *command, const char *type) {
         return NULL;
     }
 
+    /* if (type[1] != 0) {
+         errno = E2BIG;
+         return NULL;
+     }*/
+
     if (type[0] != 'w' && type[0] != 'r') {
         errno = EINVAL;
         return NULL;
     }
 
 
-    if (pipe(pipe_list) == -1) {
+    if (pipe(fileDis) == -1) {
         return NULL;  //errno will be set
     }
-#if 0
+#if 1
     child_fd = tpsr(STDIN_FILENO,STDOUT_FILENO);
     parent_fd = tpsw(STDIN_FILENO,STDOUT_FILENO);
-
+#else
     if (type[0] == 'r') {
         child_fd = STDOUT_FILENO; //1 child woll be doing the writing
         parent_fd = STDIN_FILENO; //0 parent read
@@ -54,64 +55,63 @@ FILE *mypopen(const char *command, const char *type) {
         child_fd = STDIN_FILENO; //0 child doing the reading
         parent_fd = STDOUT_FILENO;//1 parent do the writing
     }
-#else
-    child_fd = tpsrw(STDIN_FILENO,STDOUT_FILENO);
-    parent_fd = !(tpsrw(STDIN_FILENO,STDOUT_FILENO));
-#endif
 
+#endif
+    //split the pipe for read and write
+    //read_fd = fileDis[0];
+    //write_fd = fileDis[1];
 
     if ((popen_pid = fork()) == -1) {
-        const int err = errno;
-        close(pipe_list[0]);
-        close(pipe_list[1]);
-        errno = err;
+        close(fileDis[0]);
+        close(fileDis[1]);
         return NULL;
     }
 
     if (popen_pid == 0) {
         // we got a child here
-        (void) pipe_list[parent_fd];
-        if (pipe_list[child_fd] != child_fd) {
+        (void)close(fileDis[parent_fd]);
 
-            if (dup2(pipe_list[child_fd], child_fd) == -1) {
-                (void) close(pipe_list[child_fd]);
+        if (fileDis[child_fd] != child_fd) {
+
+            if (dup2(fileDis[child_fd], child_fd) == -1) {
+                (void) close(fileDis[child_fd]);
                 _exit(EXIT_FAILURE);
             }
 #if 0
             if (child_fd == STDOUT_FILENO) {
-                if (dup2(pipe_list[child_fd], STDERR_FILENO) == -1){
-                    (void) close(pipe_list[child_fd]);
+                if (dup2(filedes[child_fd], STDERR_FILENO) == -1){
+                    (void) close(filedes[child_fd]);
                     _exit(EXIT_FAILURE);
                 }
 
             }
 #endif
-
-            (void) close(pipe_list[child_fd]);
+            (void) close(fileDis[child_fd]);
         }
 
-        /* if (type[0] == 'r') {
-             (void) close(pipe_list[0]);
-             if (pipe_list[1] != STDOUT_FILENO) {
 
-                 if (dup2(pipe_list[1], STDOUT_FILENO) == -1) {
-                     (void) close(pipe_list[1]);
+        /* if (type[0] == 'r') {
+             (void) close(fileDis[0]);
+             if (fileDis[1] != STDOUT_FILENO) {
+
+                 if (dup2(fileDis[1], STDOUT_FILENO) == -1) {
+                     (void) close(fileDis[1]);
                      exit(EXIT_FAILURE);
                  }
              }
 
          } else {
-             (void) close(pipe_list[1]);
-             if (pipe_list[0] != STDIN_FILENO) {
+             (void) close(fileDis[1]);
+             if (fileDis[0] != STDIN_FILENO) {
 
-                 if (dup2(pipe_list[0], STDIN_FILENO) == -1) {
-                     (void) close(pipe_list[0]);
+                 if (dup2(fileDis[0], STDIN_FILENO) == -1) {
+                     (void) close(fileDis[0]);
                      exit(EXIT_FAILURE);
                  }
              }
          }*/
 
-        //(void) close(pipe_list[1]);
+        //(void) close(fileDis[1]);
 
 
         (void) execl(shell_path, "sh", "-c", command, (char *) NULL);
@@ -119,9 +119,9 @@ FILE *mypopen(const char *command, const char *type) {
 
     } else {
 
-        (void) close(pipe_list[child_fd]);
-        if ((fp_stream = fdopen(pipe_list[parent_fd], type)) == NULL) {
-            (void) close(pipe_list[parent_fd]);
+        (void) close(fileDis[child_fd]);
+        if ((fp_stream = fdopen(fileDis[parent_fd], type)) == NULL) {
+            (void) close(fileDis[parent_fd]);
             return NULL;
         }
 
